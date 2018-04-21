10f45f4de15c5a284efeb197e9e3f0772fee08aa
7IN0SAN9 <me@7in0.me>
2018-04-21 13:30:54 +0800

    fix test

--- tests/CoordTest.php
+++ tests/CoordTest.php
@@ -25,17 +25,17 @@ class CoordTest extends TestCase
     public function testTransformFromBd09()
     {
         $coord = new Coord(116.404, 39.915, Coord::BD09);
-        $this->assertEquals('116.39138369951,39.907253214522', $coord->copy()->toWgs84());
-        $this->assertEquals('116.39762729119,39.908656739576', $coord->copy()->toGcj02());
+        $this->assertEquals('116.39175825601,39.907541664069', (string)$coord->copy()->toWgs84());
+        $this->assertEquals('116.39762729119,39.908656739576', (string)$coord->copy()->toGcj02());
         $this->assertEquals($coord, $coord->copy()->toBd09());
     }
 
     public function testTransformFromGcj02()
     {
         $coord = new Coord(116.404, 39.915, Coord::GCJ02);
-        $this->assertEquals('116.39775550083,39.913595718498', $coord->copy()->toWgs84());
+        $this->assertEquals('116.39813053332,39.913884507655', (string)$coord->copy()->toWgs84());
         $this->assertEquals($coord, $coord->copy()->toGcj02());
-        $this->assertEquals('116.41036949371,39.92133699351', $coord->copy()->toBd09());
+        $this->assertEquals('116.41036949371,39.92133699351', (string)$coord->copy()->toBd09());
 
         $coord = new Coord(35.765, 140.386, Coord::GCJ02);
         $this->assertEquals($coord, $coord->copy()->toWgs84());
@@ -45,8 +45,8 @@ class CoordTest extends TestCase
     {
         $coord = new Coord(116.404, 39.915, Coord::WGS84);
         $this->assertEquals($coord, $coord->copy()->toWgs84());
-        $this->assertEquals('116.41024449917,39.916404281502', $coord->copy()->toGcj02());
-        $this->assertEquals('116.41662724379,39.922699552216', $coord->copy()->toBd09());
+        $this->assertEquals('116.40986946668,39.916115492345', (string)$coord->copy()->toGcj02());
+        $this->assertEquals('116.41625121875,39.922414273244', (string)$coord->copy()->toBd09());
         $this->assertEquals($coord->copy()->toGcj02(), $coord->copy()->to(Coord::GCJ02));
         $this->assertEquals($coord->copy()->toBd09(), $coord->copy()->to(Coord::BD09));
 
